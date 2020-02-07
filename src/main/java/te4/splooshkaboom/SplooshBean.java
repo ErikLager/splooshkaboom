aed3fcc2305a4b139a3b1f07eb01df5280d2274f
Erik <43432787+ErikLager@users.noreply.github.com>
2020-02-07 13:47:35 +0100

    .

--- src/main/java/te4/splooshkaboom/SplooshBean.java
+++ src/main/java/te4/splooshkaboom/SplooshBean.java
@@ -5,24 +5,45 @@
  */
 package te4.splooshkaboom;
 
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
 /**
  *
  * @author L
  */
-@ServerEndpoint("splooshkaboom")
+//Gör ship123 till json data
+@ServerEndpoint("/endpoint")
 public class SplooshBean {
 
-    public static void main(String[] args) {
+    static Set<Session> sessions = new HashSet<>();
+
+    @OnOpen
+    public void open(Session user) {
+        sessions.add(user);
+        sessions.add(user);
+    }
+
+    @OnMessage
+    public void onMessage(String Ship, Session user) throws IOException {
         Ship ship1 = new Ship(setpos(), setpos());
         Ship ship2 = new Ship(setpos(), setpos());
         Ship ship3 = new Ship(setpos(), setpos());
-        
-//        Gör ship123 till json data
+
     }
 
     static int setpos() {
         return (int) ((Math.floor(Math.random() * 10)) * 50);
     }
 }
+
+//}
